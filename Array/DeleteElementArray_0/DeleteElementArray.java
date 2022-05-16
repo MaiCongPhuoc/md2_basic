01b4a1adbd81619bf5499444414e23fa29d80f63
MaiCongPhuoc <maiphuoc244@gmail.com>
2022-05-16 13:53:43 +0700

    edit

--- Array/DeleteElementArray_0/DeleteElementArray.java
+++ Array/DeleteElementArray_0/DeleteElementArray.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class DeleteElementArray {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter a element need detele: ");
         int x = scanner.nextInt();
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] == x) {
-                array[i] = 1;
+                array[i] = array[i + 1];
             }
+            array[array.length - 1] = 0;
             System.out.print(array[i] + " ");
         }
     }
--- Introduction to Java/bai9/ConvertToString.java
+++ Introduction to Java/bai9/ConvertToString.java
@@ -4,32 +4,141 @@ import java.util.Scanner;
 
 public class ConvertToString {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n;
-        String str;
-        System.out.println("nhập số muốn chuyển thành chuỗi.");
-        n = scanner.nextInt();
-        switch (n) {
-            case 1:str = "One";
-                break;
-            case 2:str = "Two";
-                break;
-            case 3:str = "three";
-                break;
-            case 4:str = "four";
-                break;
-            case 5:str = "Six";
-                break;
-            case 6:str = "Six";
-                break;
-            case 7:str = "Seven";
-                break;
-            case 8:str = "Eight";
-                break;
-            case 9:str = "Nine";
-                break;
-            default: str = "out of ability";
+        int number = 101;
+        int first = number / 100;//2
+        int second = (number % 100) / 10;//7
+        int third = number % 10;//6
+        String result = null;
+        //if number < 10
+        if (number < 10) {
+            result =  one2Nine(third);
+            System.out.println(result);
         }
-        System.out.printf("%s %s", str, str);
+        if (number >= 10 && number < 20) {
+            result =  _10_to_19(third);
+            System.out.println(result);
+        }
+        if (number > 19 && number < 100) {
+            result = _20_to_99(second);
+            if(third != 0) {
+                result += " " + one2Nine(third);
+            }
+            System.out.println(result);
+        }
+        if (number > 99) {
+            if(first != 0) {
+                result = one2Nine(first) + " hundred ";
+            }
+            if(second !=0) {
+                result += _10_to_19(second) + " ";
+            }
+            result += one2Nine(third);
+        }
+        System.out.println(result);
+    }
+    public static String _20_to_99(int number) {
+        String second = null;
+        switch (number) {
+            case 2:
+                second = "Twenty";
+                break;
+            case 3:
+                second = "Thirty";
+                break;
+            case 4:
+                second = "Forty";
+                break;
+            case 5:
+                second = "Fifty";
+                break;
+            case 6:
+                second = "Sixty";
+                break;
+            case 7:
+                second = "Seventy";
+                break;
+            case 8:
+                second = "Eighty";
+                break;
+            case 9:
+                second = "Ninety";
+                break;
+        }
+
+        return second;
+    }
+
+    public static String _10_to_19(int number) {
+        String special = null;
+        switch (number) {
+            case 0:
+                special = "Ten";
+                break;
+            case 1:
+                special = "Eleven";
+                break;
+            case 2:
+                special = "Twelve";
+                break;
+            case 3:
+                special = "Thirteen";
+                break;
+            case 4:
+                special = "Fourteen";
+                break;
+            case 5:
+                special = "Fifteen";
+                break;
+            case 6:
+                special = "Sixteen";
+                break;
+            case 7:
+                special = "Seventeen";
+                break;
+            case 8:
+                special = "Eighteen";
+                break;
+            case 9:
+                special = "Nineteen";
+                break;
+        }
+        return special;
+    }
+
+    public static String one2Nine(int number) {
+        String donviStr = null;
+        switch (number) {
+            case 0:
+                donviStr = "Zero";
+                break;
+            case 1:
+                donviStr = "One";
+                break;
+            case 2:
+                donviStr = "Two";
+                break;
+            case 3:
+                donviStr = "Three";
+                break;
+            case 4:
+                donviStr = "Four";
+                break;
+            case 5:
+                donviStr = "Five";
+                break;
+            case 6:
+                donviStr = "Six";
+                break;
+            case 7:
+                donviStr = "Seven";
+                break;
+            case 8:
+                donviStr = "Eight";
+                break;
+            case 9:
+                donviStr = "Nine";
+                break;
+        }
+        return donviStr;
     }
 }
